02da1aeee85e341c339502a16b2e6915a3e0684b
Hannes Donner <hannes@opensuse.fritz.box>
2024-05-26 12:45:12 +0200

    initial commit.

---
+++ src/main/java/de/htw/saplantservice/SaPlantServiceApplication.java
@@ -0,0 +1,13 @@
+package de.htw.saplantservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaPlantServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaPlantServiceApplication.class, args);
+    }
+
+}
