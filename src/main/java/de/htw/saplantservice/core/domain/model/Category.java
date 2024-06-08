0c3cb3bd59e9c4bd408c39307d73fae79d0e2bc2
Marcibouz <marcio_holz@gmx.de>
2024-06-08 16:17:52 +0200

    finished model classes and added dependencies for validation in pom

--- src/main/java/de/htw/saplantservice/core/domain/model/Category.java
+++ src/main/java/de/htw/saplantservice/core/domain/model/Category.java
@@ -1,4 +1,8 @@
 package de.htw.saplantservice.core.domain.model;
 
+import lombok.Getter;
+
+@Getter
 public enum Category {
+    GARTENPFLANZE, ZIMMERPFLANZE, HECKENPFLANZE, BAUM
 }
--- src/main/java/de/htw/saplantservice/core/domain/model/Plant.java
+++ src/main/java/de/htw/saplantservice/core/domain/model/Plant.java
@@ -1,12 +1,15 @@
 package de.htw.saplantservice.core.domain.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity //Hibernate
-@Table  //Table in Data Base
+import javax.validation.constraints.Size;
+
+@Entity //Marks class as database Entity
+@Table  //Table in Data Base (here you can name the table)
 @Getter
 @Setter
 @NoArgsConstructor
@@ -23,8 +26,60 @@ public class Plant {
             generator = "plant_sequence"
     )
     private Long id;
+
+    @NotNull
+    @Size(max = 50)
     private String name;
+
+    @NotNull
+    @Size(max = 50)
     private String latinName;
-    private Height height;
+
+    @NotNull
+    private String description;
+
+    @NotNull
+    private float price;
+
+    @Enumerated(EnumType.STRING)
     private Category category;
+
+    @Enumerated(EnumType.STRING)
+    private Height height;
+
+    @Enumerated(EnumType.STRING)
+    private WaterDemand waterDemand;
+
+    private String imageLink;
+
+    private int amount;
+
+    public Plant(String name, String latinName, String description, float price, Category category, Height height,
+                 WaterDemand waterDemand, String imageLink, int amount) {
+        this.name = name;
+        this.latinName = latinName;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+        this.height = height;
+        this.waterDemand = waterDemand;
+        this.imageLink = imageLink;
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Plant{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", latinName='" + latinName + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", category=" + category +
+                ", height=" + height +
+                ", waterDemand=" + waterDemand +
+                ", imageLink='" + imageLink + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
 }
