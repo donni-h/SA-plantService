3b2fb02229abf6273093bab27b1b9adac0870320
Marcibouz <marcio_holz@gmx.de>
2024-07-07 16:12:37 +0200

    PlantRepositoryTest and pom.xml changes

---
+++ src/test/java/de/htw/saplantservice/integrationTests/PlantRepositoryTest.java
@@ -0,0 +1,98 @@
+package de.htw.saplantservice.integrationTests;
+
+import de.htw.saplantservice.core.domain.model.Category;
+import de.htw.saplantservice.core.domain.model.Height;
+import de.htw.saplantservice.core.domain.model.Plant;
+import de.htw.saplantservice.core.domain.model.WaterDemand;
+import de.htw.saplantservice.core.domain.service.interfaces.IPlantRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.List;
+
+import static junit.framework.TestCase.*;
+
+@Testcontainers
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class PlantRepositoryTest {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>( "postgres:latest");
+
+    @Autowired
+    private IPlantRepository iPlantRepository;
+
+    @Test
+    void connectionEstablished(){
+        assertTrue(postgres.isCreated());
+        assertTrue(postgres.isRunning());
+    }
+
+    @BeforeEach
+    void setUp(){
+        Plant plant = new Plant("Pflanzus Longus", "Pflanzisimus Longimus", "coole Pflanze",
+                5.40f, Category.ZIMMERPFLANZE, Height.L, WaterDemand.MEDIUM, "cooles Bild", 15);
+        iPlantRepository.save(plant);
+    }
+
+    @Test
+    void findByNameTest_PlantExists(){
+        List<Plant> plants = iPlantRepository.findByName("Pflanzus Longus");
+
+        assertTrue(plants.size() == 1);
+
+        Plant plantByName = plants.get(0);
+        assertNotNull(plantByName);
+        assertEquals("Pflanzus Longus", plantByName.getName());
+        assertEquals("Pflanzisimus Longimus", plantByName.getLatinName());
+        assertEquals("coole Pflanze", plantByName.getDescription());
+        assertEquals(5.40f, plantByName.getPrice());
+        assertEquals(Category.ZIMMERPFLANZE, plantByName.getCategory());
+        assertEquals(Height.L, plantByName.getHeight());
+        assertEquals(WaterDemand.MEDIUM, plantByName.getWaterDemand());
+        assertEquals("cooles Bild", plantByName.getImageLink());
+        assertEquals(Integer.valueOf(15), plantByName.getAmount());
+    }
+
+    @Test
+    void findByNameTest_PlantDoesNotExist(){
+        List<Plant> plants = iPlantRepository.findByName("Kebabpflanze");
+
+        assertTrue(plants.isEmpty());
+    }
+
+    @Test
+    void findByCategoryTest_PlantExists(){
+        List<Plant> plants = iPlantRepository.findByCategory(Category.ZIMMERPFLANZE);
+
+        assertTrue(plants.size() == 1);
+
+        Plant plantByName = plants.get(0);
+        assertNotNull(plantByName);
+        assertEquals("Pflanzus Longus", plantByName.getName());
+        assertEquals("Pflanzisimus Longimus", plantByName.getLatinName());
+        assertEquals("coole Pflanze", plantByName.getDescription());
+        assertEquals(5.40f, plantByName.getPrice());
+        assertEquals(Category.ZIMMERPFLANZE, plantByName.getCategory());
+        assertEquals(Height.L, plantByName.getHeight());
+        assertEquals(WaterDemand.MEDIUM, plantByName.getWaterDemand());
+        assertEquals("cooles Bild", plantByName.getImageLink());
+        assertEquals(Integer.valueOf(15), plantByName.getAmount());
+    }
+
+    @Test
+    void findByCategoryTest_PlantDoesNotExists(){
+        List<Plant> plants = iPlantRepository.findByCategory(Category.BAUM);
+
+        assertTrue(plants.isEmpty());
+    }
+}
