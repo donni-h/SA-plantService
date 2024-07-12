d3d13249f6ce6006f4576d6491513591192fdcc4
hannes <56559005+donni-h@users.noreply.github.com>
2024-07-12 10:01:20 +0200

    Added Conversion from keycloak JWT to Spring Roles

--- src/main/java/de/htw/saplantservice/port/user/security/configuration/SecurityConfig.java
+++ src/main/java/de/htw/saplantservice/port/user/security/configuration/SecurityConfig.java
@@ -1,14 +1,18 @@
 package de.htw.saplantservice.port.user.security.configuration;
 
+import de.htw.saplantservice.port.user.security.KeycloakJwtAuthenticationConverter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -18,7 +22,7 @@ public class SecurityConfig {
                         .authenticated()
                 )
                 .oauth2ResourceServer((oauth2) -> oauth2
-                        .jwt(Customizer.withDefaults())
+                        .jwt(token -> token.jwtAuthenticationConverter(new KeycloakJwtAuthenticationConverter()))
                 );
         return http.build();
     }
