2479064b8ed8ca78a8e9ca85034321a66190b9f2
Hannes <56559005+donni-h@users.noreply.github.com>
2024-07-15 23:33:31 +0200

    manage authentication for roles

--- src/main/java/de/htw/saplantservice/port/config/SecurityConfig.java
+++ src/main/java/de/htw/saplantservice/port/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import de.htw.saplantservice.port.security.KeycloakJwtAuthenticationConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,8 +19,11 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .anyRequest()
-                        .authenticated()
+                        .requestMatchers(HttpMethod.GET).permitAll()
+                        .requestMatchers(HttpMethod.POST).hasRole("content")
+                        .requestMatchers(HttpMethod.PUT).hasRole("content")
+                        .requestMatchers(HttpMethod.DELETE).hasRole("content")
+                        .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer((oauth2) -> oauth2
                         .jwt(token -> token.jwtAuthenticationConverter(new KeycloakJwtAuthenticationConverter()))
